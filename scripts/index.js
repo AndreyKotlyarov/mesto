dad858ff8fa434b3689e9c32e147755fc0d25da6
Andrey Kotlyarov <kotlyarov.andrey01@gmail.com>
2022-08-23 18:49:00 +0900

    fix: fix

--- scripts/index.js
+++ scripts/index.js
@@ -1,35 +1,30 @@
 let popup = document.querySelector(".pop-up");
 let editButton = document.querySelector(".profile__edit-button");
 let closeButton = popup.querySelector(".pop-up__close-button");
-// let togglePopup = function () {
-//   popup.classList.toggle("pop-up_opened");
-// };
+let formElement = document.querySelector(".pop-up__form");
+let nameInput = formElement.querySelector(".pop-up__input_type_name");
+let jobInput = formElement.querySelector(".pop-up__input_type_job");
+let profileName = document.querySelector(".profile__name");
+let profileCaption = document.querySelector(".profile__caption");
+
 let openPopup = function () {
-  if (popup.classList.contains("pop-up_opened") === false) {
-    popup.classList.add("pop-up_opened");
-  }
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileCaption.textContent;
+  popup.classList.add("pop-up_opened");
 };
 
 let closePopup = function () {
-  if (popup.classList.contains("pop-up_opened") === true) {
-    popup.classList.remove("pop-up_opened");
-  }
+  popup.classList.remove("pop-up_opened");
 };
 
 editButton.addEventListener("click", openPopup);
 closeButton.addEventListener("click", closePopup);
 
-let formElement = document.querySelector(".pop-up__form");
-let nameInput = formElement.querySelector(".pop-up__input_name");
-let jobInput = formElement.querySelector(".pop-up__input_job");
-
 function formSubmitHandler(evt) {
   evt.preventDefault();
-
-  let profileName = document.querySelector(".profile__name");
-  let profileCaption = document.querySelector(".profile__caption");
   profileName.textContent = nameInput.value;
   profileCaption.textContent = jobInput.value;
   closePopup();
 }
+
 formElement.addEventListener("submit", formSubmitHandler);
